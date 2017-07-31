35450e93369d67dfc8e254fad39e6a6eb762590f
Alex Close <ajclose@gmail.com>
2017-07-31 11:44:53 -0500

    Add user with auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'questions#index'
+
+  get 'sessions/new' => 'sessions#new', as: :login
+  post 'sessions/new' => 'sessions#create'
+  delete 'sessions' => 'sessions#destroy', as: :logout
+
+  get 'register' => 'users#new', as: :registration
+  post 'register' => 'users#create'
 end
